21b06091b21566f184dcd2cf3ef8c3024b01802e
César Manzo <hr241410@gmail.com>
2016-01-03 12:38:19 -0600

    Added authors, edited controllers to see My Routes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,14 @@
 angular.module('app.controllers', [])
   
-.controller('myRoutesCtrl', function($scope) {
-
+.controller('myRoutesCtrl', function($scope, $http) {
+    $http.get('../myroutes.json').then(function(response) {
+        $scope.routes = response.data;
+        console.log('Success', response.data);
+    // For JSON responses, response.data contains the result
+      }, function(err) {
+        console.error('ERR', err);
+    // err.status will contain the status code
+  })
 })
    
 .controller('mapCtrl', function($scope) {
